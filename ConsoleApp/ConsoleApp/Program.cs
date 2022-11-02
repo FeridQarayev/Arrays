bc9b873278df1b515f91105fbd05c2af2e262715
FeridQarayev <tu7he6n9s@code.edu.az>
2022-11-03 02:14:49 +0400

    Add ConsoleApp file

---
+++ ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿
+
+int[] ints = new int[] { 1, 2, 3, 4 };
+Console.WriteLine("before");
+for (int i = 0; i < ints.Length; i++)   //// O(N) — Linear Complexity
+{
+    Console.Write(ints[i] + ", ");
+}
+
+Console.WriteLine("\n\nafter");
+for (int i = 0; i < ints.Length; i++)   //// O(N) — Linear Complexity
+{
+    ints[i] = ints[i] + 1;
+    Console.Write(ints[i] + ", ");
+}

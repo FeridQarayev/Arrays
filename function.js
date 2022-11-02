f14fadb85eea40c7d8ee9e2a41e11b056078d6c2
FeridQarayev <tu7he6n9s@code.edu.az>
2022-11-03 02:45:31 +0400

    update function js file

---
+++ function.js
@@ -0,0 +1,27 @@
+function sum(a,b){
+    return a+b;
+}
+
+function multiply(a,b){
+    return a*b;
+}
+
+function subtract(a,b){
+    return a-b;
+}
+
+function divide(a,b){
+    return b/a;
+}
+
+let result=sum(3,1)
+console.log(result)
+
+result=multiply(3,1)
+console.log(result)
+
+result=subtract(3,1)
+console.log(result)
+
+result=divide(3,1)
+console.log(result)
